7d78c1b39e23a9ab04d860687a54a5f88dc6739a
shubhamkr95 <sahooshubham124@gmail.com>
2022-05-18 14:42:25 +0530

    Added new address

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -2,7 +2,7 @@ import abi from "./LazyNFT.json";
 import { ethers } from "ethers";
 
 export const Abi = abi.abi;
-const contractAddress = "0xb258E0301A63b0DCCaaD1e2D60D941fbFD3A321E";
+const contractAddress = "0x36b1A0bb9A071289E7743B164765A6aEE695F636";
 export const provider = new ethers.providers.Web3Provider(window.ethereum);
 export const signer = provider.getSigner();
 export const url = "https://nameless-headland-03040.herokuapp.com/lazyApi/";
